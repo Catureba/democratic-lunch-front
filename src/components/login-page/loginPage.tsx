3a8b9374c4a6b2114f2a1dc6df0a809f4802b957
catureba <caturebadev@gmail.com>
2023-02-08 16:59:47 -0300

    Create Login Page Components and make a HTML

--- src/components/login-page/loginPage.tsx
+++ src/components/login-page/loginPage.tsx
@@ -1,10 +1,26 @@
 
 import styles from './loginPage.module.css'
 
-function LoginPage(){
-    return(
+function LoginPage() {
+    return (
         <div className={styles.loginPageConteiner}>
-            <h1>hello world</h1>
+            <div className={styles.box}>
+                <div className={styles.boxLeft}>
+                    <h1>Esquerda</h1>
+                </div>
+                <div className={styles.boxRight}>
+                    <h1>Direita</h1>
+                    <div className={styles.formBox}>
+                        <form action="">
+                            <input placeholder='Email' type="email" name="" id="" />
+                            <input placeholder='Senha' type="password" />
+                            <button type="submit" >Entrar</button>
+                            <span className={styles.line}></span>
+                        </form>
+                        <button>Criar Conta</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
