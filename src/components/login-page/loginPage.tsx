995e57bd0a4bb3cf179502c10864910047071f79
catureba <caturebadev@gmail.com>
2023-02-09 12:08:50 -0300

    Login Page Upload

--- src/components/login-page/loginPage.tsx
+++ src/components/login-page/loginPage.tsx
@@ -6,18 +6,21 @@ function LoginPage() {
         <div className={styles.loginPageConteiner}>
             <div className={styles.box}>
                 <div className={styles.boxLeft}>
-                    <h1>Esquerda</h1>
+                    <div className={styles.boxLeftIntern}>
+                        <h1>Democratic Lunch</h1>
+                        <img src="./democratic-lunch-tela-login.jpg" alt="" />
+                    </div>
+
                 </div>
                 <div className={styles.boxRight}>
-                    <h1>Direita</h1>
                     <div className={styles.formBox}>
                         <form action="">
-                            <input placeholder='Email' type="email" name="" id="" />
+                            <input placeholder='Email' type="email" name="" id="" className={styles.inputBox1} />
                             <input placeholder='Senha' type="password" />
-                            <button type="submit" >Entrar</button>
+                            <button type="submit" >Login</button>
                             <span className={styles.line}></span>
                         </form>
-                        <button>Criar Conta</button>
+                        <button>Cadastrar</button>
                     </div>
                 </div>
             </div>
