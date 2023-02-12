d28de6de09d45754122d646117d2ba8255b5ecec
catureba <caturebadev@gmail.com>
2023-02-12 00:45:19 -0300

    footer update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
-
 import Layout from "../pages/Layout";
-
 import LoginPage from "../pages/LoginPage";
 import SignUp from "../pages/SignUp";
 
