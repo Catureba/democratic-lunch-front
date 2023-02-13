d1784d41d1a1be410a6df92b3e9c2dd44b802185
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-02-13 10:45:22 -0300

    creating tests files for the components

---
+++ src/pages/SignUp/signUp.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from '.';
+import '@testing-library/jest-dom/extend-expect';
+import { AxiosResponse } from 'axios';
+
+describe("Página de Cadastro", () =>{
+ 
+});
